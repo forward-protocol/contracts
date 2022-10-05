0122b1be935d379dd79061abba27f29a61ebf886
George Roman <george.roman.99@gmail.com>
2022-10-05 15:11:33 +0200

    perf: improve gas costs

--- src/Forward.sol
+++ src/Forward.sol
@@ -6,7 +6,6 @@ import {Clones} from "openzeppelin/proxy/Clones.sol";
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 import {IERC721} from "openzeppelin/token/ERC721/IERC721.sol";
 import {IERC1155} from "openzeppelin/token/ERC1155/IERC1155.sol";
-import {ECDSA} from "openzeppelin/utils/cryptography/ECDSA.sol";
 
 import {Vault} from "./Vault.sol";
 import {IRoyaltyEngine} from "./interfaces/IRoyaltyEngine.sol";
@@ -312,11 +311,7 @@ contract Forward is Ownable {
 
         // Ensure the maker's signature is valid
         bytes32 eip712Hash = _getEIP712Hash(getBidHash(bid));
-        // TODO: Add support for EIP2098 and EIP1271 signatures
-        address signer = ECDSA.recover(eip712Hash, details.signature);
-        if (signer != maker) {
-            revert InvalidSignature();
-        }
+        _verifySignature(maker, eip712Hash, details.signature);
 
         BidStatus memory bidStatus = bidStatuses[eip712Hash];
         // Ensure the bid is not cancelled
@@ -466,6 +461,107 @@ contract Forward is Ownable {
 
         if (!isValid) {
             revert InvalidCriteriaProof();
+        }
+    }
+
+    // Taken from:
+    // https://github.com/ProjectOpenSea/seaport/blob/e4c6e7b294d7b564fe3fe50c1f786cae9c8ec575/contracts/lib/SignatureVerification.sol#L31-L35
+    function _verifySignature(
+        address signer,
+        bytes32 digest,
+        bytes memory signature
+    ) internal view {
+        bool success;
+
+        // TODO: Add support for EIP1271 contract signatures
+        assembly {
+            // Ensure that first word of scratch space is empty
+            mstore(0, 0)
+
+            let v
+            let signatureLength := mload(signature)
+
+            // Get the pointer to the value preceding the signature length
+            let wordBeforeSignaturePtr := sub(signature, 0x20)
+
+            // Cache the current value behind the signature to restore it later
+            let cachedWordBeforeSignature := mload(wordBeforeSignaturePtr)
+
+            // Declare lenDiff + recoveredSigner scope to manage stack pressure
+            {
+                // Take the difference between the max ECDSA signature length and the actual signature length
+                // Overflow desired for any values > 65
+                // If the diff is not 0 or 1, it is not a valid ECDSA signature
+                let lenDiff := sub(65, signatureLength)
+
+                let recoveredSigner
+
+                // If diff is 0 or 1, it may be an ECDSA signature, so try to recover signer
+                if iszero(gt(lenDiff, 1)) {
+                    // Read the signature `s` value
+                    let originalSignatureS := mload(add(signature, 0x40))
+
+                    // Read the first byte of the word after `s`
+                    // If the signature is 65 bytes, this will be the real `v` value
+                    // If not, it will need to be modified - doing it this way saves an extra condition
+                    v := byte(0, mload(add(signature, 0x60)))
+
+                    // If lenDiff is 1, parse 64-byte signature as ECDSA
+                    if lenDiff {
+                        // Extract yParity from highest bit of vs and add 27 to get v
+                        v := add(shr(0xff, originalSignatureS), 27)
+
+                        // Extract canonical s from vs, all but the highest bit
+                        // Temporarily overwrite the original `s` value in the signature
+                        mstore(
+                            add(signature, 0x40),
+                            and(
+                                originalSignatureS,
+                                0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+                            )
+                        )
+                    }
+
+                    // Temporarily overwrite the signature length with `v` to conform to the expected input for ecrecover
+                    mstore(signature, v)
+
+                    // Temporarily overwrite the word before the length with `digest` to conform to the expected input for ecrecover
+                    mstore(wordBeforeSignaturePtr, digest)
+
+                    // Attempt to recover the signer for the given signature
+                    // Do not check the call status as ecrecover will return a null address if the signature is invalid
+                    pop(
+                        staticcall(
+                            gas(),
+                            1, // Call ecrecover precompile
+                            wordBeforeSignaturePtr, // Use data memory location
+                            0x80, // Size of digest, v, r, and s
+                            0, // Write result to scratch space
+                            0x20 // Provide size of returned result
+                        )
+                    )
+
+                    // Restore cached word before signature
+                    mstore(wordBeforeSignaturePtr, cachedWordBeforeSignature)
+
+                    // Restore cached signature length
+                    mstore(signature, signatureLength)
+
+                    // Restore cached signature `s` value
+                    mstore(add(signature, 0x40), originalSignatureS)
+
+                    // Read the recovered signer from the buffer given as return space for ecrecover
+                    recoveredSigner := mload(0)
+                }
+
+                // Set success to true if the signature provided was a valid ECDSA signature and the signer is not the null address
+                // Use gt instead of direct as success is used outside of assembly
+                success := and(eq(signer, recoveredSigner), gt(signer, 0))
+            }
+        }
+
+        if (!success) {
+            revert InvalidSignature();
         }
     }
 }
